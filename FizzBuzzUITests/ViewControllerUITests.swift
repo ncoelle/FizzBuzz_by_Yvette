39005365143aea56e9febdfd6b77910bced62912
Niels Cölle <niels@coelle-online.com>
2018-01-02 12:40:01 +0100

    Test refactored for using Accessibility ID

--- FizzBuzzUITests/ViewControllerUITests.swift
+++ FizzBuzzUITests/ViewControllerUITests.swift
@@ -21,8 +21,11 @@ class ViewControllerUITests: XCTestCase {
   }
 
   func testTapNumberButtonIncrementsScore() {
-	XCUIApplication().buttons["0"].tap()
-	let newScore = XCUIApplication().buttons["1"].label
-	XCTAssertEqual(newScore, "1")
+    let app = XCUIApplication()
+    let numberButton = app.buttons["numberButton"]
+
+    numberButton.tap()
+    let newScore = numberButton.label
+    XCTAssertEqual(newScore, "1")
   }
 }
